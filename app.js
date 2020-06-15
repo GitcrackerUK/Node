ebccae599c6b47272e1312de6897c9d549a2fc52
gitcrackerUK <pfsiwek@gmail.com>
2020-06-15 14:26:29 +0100

    added express and set up server

--- app.js
+++ app.js
@@ -1,15 +1,7 @@
-const http = require('http');
+const express = require('express');
+const app = express();
+const port = 3000;
 
-const hostname = '127.0.0.1';
+app.get('/',(req,res)=> res.send('Hello world!!'));
 
-const port = 5500;
-
-const server = http.createServer((req,res)=>{
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World');
-});
-
-server.listen(port,hostname, () =>{
-    console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.listen(port,()=>console.log(`Example app listening at http://localhost:${port}`))
